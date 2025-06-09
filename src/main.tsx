0fddec92d2a848798b787b5d3a92f29267515c1c
8 <sapoznikvladislav58@gmail.com>
2025-06-09 17:30:10 +0300

    Made small fix

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
