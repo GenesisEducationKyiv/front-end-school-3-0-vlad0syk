b9fcd9187c812a01dbf493b6e70b79c5cb34526d
8 <sapoznikvladislav58@gmail.com>
2025-06-14 20:21:56 +0300

    Moved usePagination hook to another file

--- src/hooks/useFiltersState.ts
+++ src/hooks/useFiltersState.ts
@@ -139,34 +139,3 @@ export function useFiltersState() {
     hasActiveFilters
   };
 }
-
-export function usePagination() {
-  const { params, setPage, setLimit } = useFiltersState();
-  
-  const goToNextPage = useCallback(() => {
-    setPage((params.page || 1) + 1);
-  }, [params.page, setPage]);
-  
-  const goToPrevPage = useCallback(() => {
-    const currentPage = params.page || 1;
-    if (currentPage > 1) {
-      setPage(currentPage - 1);
-    }
-  }, [params.page, setPage]);
-  
-  const goToPage = useCallback((page: number) => {
-    if (page > 0) {
-      setPage(page);
-    }
-  }, [setPage]);
-
-  return {
-    currentPage: params.page || 1,
-    limit: params.limit || 10,
-    setPage: goToPage,
-    setLimit,
-    goToNextPage,
-    goToPrevPage,
-    canGoPrev: (params.page || 1) > 1
-  };
-}
