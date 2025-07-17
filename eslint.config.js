230703b8ed8d058cc72c039de05771d74afaa13e
vlad0syk <sapoznikvladislav58@gmail.com>
2025-07-17 10:29:58 +0300

    Deleted nx and reinstalled storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,60 +1,59 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: [
-      'dist',
-      'node_modules',
-      '.nx/',
-      'playwright.config.ts',
-      'vitest.config.ts',
-    ],
+export default tseslint.config({
+  ignores: [
+    'dist',
+    'node_modules',
+    '.nx/',
+    'playwright.config.ts',
+    'vitest.config.ts',
+  ],
+}, {
+  files: ['**/*.{ts,tsx}'],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.strict,
+    // ...tseslint.configs.strictTypeChecked,
+    // ...tseslint.configs.stylisticTypeChecked,
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    globals: globals.browser,
   },
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strict,
-      // ...tseslint.configs.strictTypeChecked,
-      // ...tseslint.configs.stylisticTypeChecked,
-    ],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.app.json', './tsconfig.node.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-    },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
   },
-  {
-    files: ['**/*.js'],
-    extends: [js.configs.recommended],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: globals.node,
-    },
-  }
-);
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+  },
+}, {
+  files: ['**/*.js'],
+  extends: [js.configs.recommended],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    globals: globals.node,
+  },
+}, storybook.configs["flat/recommended"]);
